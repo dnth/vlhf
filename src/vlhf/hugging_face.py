635f4aa58a20b2e30edb3d1c4f5d2f3199016d1c
dnth <dickson.neoh@gmail.com>
2024-07-30 22:11:29 +0800

    forward reference

--- src/vlhf/hugging_face.py
+++ src/vlhf/hugging_face.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 import shutil
 import uuid
@@ -7,28 +9,35 @@ from huggingface_hub import HfApi
 from loguru import logger
 from tqdm.auto import tqdm
 
-from .visual_layer import VisualLayer
-
 
 class HuggingFace:
     def __init__(self, token: str = None) -> None:
         self.api = HfApi(token=token)
 
     def download_dataset(
-        self, dataset_id: str, save_path: str = None, **dataset_kwargs
-    ):
+        self,
+        dataset_id: str,
+        save_path: str = None,
+        image_key="image",
+        **dataset_kwargs,
+    ) -> None:
+        def add_image_filename(examples):
+            image_paths = [img["path"] for img in examples[image_key]]
+            examples["image_filename"] = image_paths
+            return examples
+
         self.save_path = save_path or dataset_id
         logger.info(f"Downloading dataset {dataset_id} and saving to {self.save_path}")
         self.dataset = load_dataset(dataset_id, split="all", **dataset_kwargs)
 
-        self.dataset = self.dataset.cast_column("image", Image(decode=False))
-        self.dataset = self.dataset.map(self.__add_image_filename, batched=True)
-        self.dataset = self.dataset.cast_column("image", Image(decode=True))
+        self.dataset = self.dataset.cast_column(image_key, Image(decode=False))
+        self.dataset = self.dataset.map(add_image_filename, batched=True)
+        self.dataset = self.dataset.cast_column(image_key, Image(decode=True))
 
         os.makedirs(self.save_path, exist_ok=True)
 
         for row in tqdm(self.dataset, desc="Saving images"):
-            image = row["image"]
+            image = row[image_key]
             image_filename = row["image_filename"]
 
             if image_filename is None:
@@ -44,17 +53,12 @@ class HuggingFace:
 
         return [d for d in datasets]
 
-    def to_vl(self, vl: VisualLayer, dataset_name: str = None):
+    def to_vl(self, vl: "VisualLayer", dataset_name: str = None) -> None:
         if dataset_name is None:
             dataset_name = self.save_path
-            
+
         self.__make_dataset_tar()
         vl.create_dataset(dataset_name, f"{self.save_path}.tar")
-
-    def __add_image_filename(self, examples):
-        image_paths = [img["path"] for img in examples["image"]]
-        examples["image_filename"] = image_paths
-        return examples
 
     def __make_dataset_tar(self):
         shutil.make_archive(self.save_path, "tar", self.save_path)
--- src/vlhf/visual_layer.py
+++ src/vlhf/visual_layer.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+
 from vl_research.sdk.dataset_api import DatasetSession
+
+
 
 class VisualLayer:
     def __init__(self, user_id: str, env: str) -> None:
@@ -8,3 +12,9 @@ class VisualLayer:
     def create_dataset(self, dataset_name: str, dataset_tar_path: str):
         session = DatasetSession(self.user_id, self.env)
         session.create_dataset_archive(dataset_name, dataset_tar_path)
+
+    def list_datasets(self):
+        raise NotImplementedError
+    
+    def to_hf(self, hf: 'HuggingFace', dataset_name: str = None):
+        raise NotImplementedError
