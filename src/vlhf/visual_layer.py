0412159d4f7a92b77934aebb519580f37ac52569
dnth <dickson.neoh@gmail.com>
2024-08-05 16:49:03 +0800

    add issues to hf upload

--- src/vlhf/visual_layer.py
+++ src/vlhf/visual_layer.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import polars as pl
-from datasets import Dataset, Features, Value
+from datasets import Dataset, Features, Sequence, Value
 from loguru import logger
 from vl_research.sdk.dataset_api import DatasetSession
 
@@ -31,8 +31,26 @@ class VisualLayer:
         """
         Pushes a dataset to the Hugging Face repository.
         """
-        dataset = Dataset.from_polars(vl_dataset_df)
-        features = Features({"image_uri": Value("string"), "label": Value("string")})
+
+        # pandas has better support in hf datasets currently
+        # so we convert the polars dataframe to pandas
+        # TODO: revisit this when polars support is better
+        pd_dataset = vl_dataset_df.to_pandas()  
+
+        dataset = Dataset.from_pandas(pd_dataset)
+        features = Features(
+            {
+                "image_uri": Value("string"),
+                "label": Value("string"),
+                "issues": [
+                    {
+                        "confidence": Value("float64"),
+                        "description": Value("string"),
+                        "issue_type": Value("string"),
+                    }
+                ],
+            }
+        )
         dataset = dataset.cast(features)
 
         logger.info(f"Pushing dataset to HF repository: {hf_repo_id}")
@@ -41,10 +59,13 @@ class VisualLayer:
     def get_dataset(self, dataset_id: str, pg_uri: str) -> pl.DataFrame:
         labels = self._get_labels(dataset_id, pg_uri)
         images = self._get_images(dataset_id, pg_uri)
+        issues = self._get_issues(dataset_id, pg_uri)
 
-        vl_dataset = images.join(
-            labels, left_on="id", right_on="image_id", how="left"
-        ).select("image_uri", "label")
+        vl_dataset = images.join(labels, left_on="id", right_on="image_id", how="left")
+
+        vl_dataset = vl_dataset.join(
+            issues, left_on="id", right_on="image_id", how="left"
+        ).select("image_uri", "label", "issues")
 
         return vl_dataset
 
@@ -65,3 +86,32 @@ class VisualLayer:
             image_id=pl.col("original_uri").str.extract("([^/\.]+)\..+$"),
             image_uri="image_uri",
         )
+
+    def _get_issues(self, dataset_id: str, pg_uri: str) -> pl.DataFrame:
+        try:
+            logger.info(f"Fetching issues for dataset: {dataset_id}")
+
+            issues = pl.read_database_uri(
+                f"SELECT * FROM image_issues WHERE dataset_id = '{dataset_id}'", pg_uri
+            )
+            logger.info(f"Retrieved {len(issues)} issues for dataset {dataset_id}")
+
+            issues_types = pl.read_database_uri("SELECT * FROM issue_type", pg_uri)
+
+            issues = issues.join(issues_types, left_on="type_id", right_on="id")
+            logger.info(
+                f"Joined issues with issue types. Final result has {len(issues)} rows"
+            )
+
+            issues = issues.filter(pl.col("cause").is_null())
+            issues = issues.select(
+                "image_id",
+                issues=pl.struct("confidence", "description", issue_type="name"),
+            )
+            issues = issues.group_by("image_id").all()
+
+            return issues
+
+        except Exception as e:
+            logger.error(f"Error retrieving issues for dataset {dataset_id}: {str(e)}")
+            raise
