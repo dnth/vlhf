d028e5e67ec065be4d2a62ab8cbca69b98e5432f
dnth <dickson.neoh@gmail.com>
2024-08-14 17:34:50 +0800

    update test

--- tests/test_hf.py
+++ tests/test_hf.py
@@ -1,27 +1,15 @@
 import pytest
-from src.vlhf.hugging_face import HuggingFace
-from unittest.mock import patch
-
-
-class MockHfApi:
-    def __init__(self, token):
-        self.token = token
+from huggingface_hub import HfApi
+from vlhf.hugging_face import HuggingFace
 
 
 @pytest.fixture
-def hugging_face_instance():
-    with patch("src.vlhf.hugging_face.HfApi", new=MockHfApi):
-        return HuggingFace(token="test_token")
+def hf_instance():
+    return HuggingFace(token="dummy_token")
 
 
-def test_hugging_face_initialization(hugging_face_instance):
-    assert hugging_face_instance.token == "test_token"
-    assert (
-        hugging_face_instance.api.token == "test_token"
-    )  # Check if api is initialized correctly
-    assert hugging_face_instance.dataset is None
-    assert hugging_face_instance.save_path is None
-    assert hugging_face_instance.image_key is None
-    assert hugging_face_instance.label_key is None
-    assert hugging_face_instance.bbox_key is None
-    assert hugging_face_instance.bbox_label_names is None
+def test_huggingface_init(hf_instance):
+    assert isinstance(hf_instance.api, HfApi)
+    assert hf_instance.token == "dummy_token"
+    assert hf_instance.dataset is None
+    assert hf_instance.save_path is None
