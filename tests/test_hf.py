16d5c61b3d7e32b224dd2d08f976e0658e4c1d30
dnth <dickson.neoh@gmail.com>
2024-08-15 13:04:54 +0800

    test is one_indexed function

--- tests/test_hf.py
+++ tests/test_hf.py
@@ -1,6 +1,9 @@
 import pytest
 from huggingface_hub import HfApi
-from vlhf.hugging_face import HuggingFace
+from vlhf.hugging_face import HuggingFace, is_one_indexed
+import pandas as pd
+from datasets import Dataset
+import os
 
 
 @pytest.fixture
@@ -13,3 +16,14 @@ def test_huggingface_init(hf_instance):
     assert hf_instance.token == "dummy_token"
     assert hf_instance.dataset is None
     assert hf_instance.save_path is None
+
+
+def test_object_label_is_one_indexed():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parquet_path = os.path.join(
+        current_dir, "sample_data_zero_indexed_object_annotations.parquet"
+    )
+
+    mock_data = pd.read_parquet(parquet_path)
+    dataset = Dataset.from_dict(mock_data)
+    assert is_one_indexed(dataset) is False
